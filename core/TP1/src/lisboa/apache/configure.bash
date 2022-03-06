623769ba31d135dd8c3e85df0396e7380d42c52c
Gabriel Fernandes <gabriel@student.dei.uc.pt>
2022-03-06 12:11:08 +0000

    Apache configs added.

---
+++ core/TP1/src/lisboa/apache/configure.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# PARTO DO PRINCIPIO QUE OS CERTIFICADOS JA FORAM CRIADOS E ESTAO NOS FOLDERS CORRETOS
+
+filedir=`dirname "$0"`
+
+# instalar o apache
+sudo apt-get install apache2
+
+# iniciar o apache
+sudo systemctl start apache2
+
+# dar enable ao modulo ssl
+sudo a2enmod ssl
+
+# dar load dos folders para os locais corretos
+    # folders html
+    sudo mkdir /var/www/server_tp1 /var/www/server_tp1_ssl
+    sudo cp -f -r $filedir/servers_html/server_tp1 /var/www/
+    sudo cp -f -r $filedir/servers_html/server_tp1_ssl /var/www/
+    
+    # folders conf do apache
+    sudo cp -f $filedir/config/http.conf $filedir/config/ssl-http.conf /etc/apache2/sites-available
+
+# dar enable aos sites no servico apache
+cd /etc/apache2/sites-available
+sudo a2ensite http.conf
+sudo a2ensite ssl-http.conf
+
+# dar reload ao apache
+sudo systemctl reload apache2
