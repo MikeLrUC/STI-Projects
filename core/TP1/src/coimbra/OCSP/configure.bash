b4499610e72c2dea501f1180861409c5e7ba0a41
Gabriel Fernandes <gabi2000mf@gmail.com>
2022-03-12 02:06:47 +0000

    TA keys for HMAC added.

--- core/TP1/src/coimbra/OCSP/configure.bash
+++ core/TP1/src/coimbra/OCSP/configure.bash
@@ -1,5 +1,5 @@
-# ligar o OCSP em Coimbra
-sudo openssl ocsp -index /etc/pki/CA/index.txt -port 81 -rsigner /etc/pki/CA/cacert.pem -rkey /etc/pki/CA/private/cakey.pem -CA /etc/pki/CA/cacert.pem -text -out log.txt
-
 # para que a vm de coimbra consiga chegar ao ocsp.server
 echo "10.8.0.1     ocsp.server" | sudo tee -a /etc/hosts
+
+# ligar o OCSP em Coimbra
+sudo openssl ocsp -index /etc/pki/CA/index.txt -port 81 -rsigner /etc/pki/CA/cacert.pem -rkey /etc/pki/CA/private/cakey.pem -CA /etc/pki/CA/cacert.pem -text -out log.txt
