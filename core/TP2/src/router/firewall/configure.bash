e1c4c84471bf429071e8f280ba2806f86262b13b
MikeLrUC <miguelrabuge12@gmail.com>
2022-04-03 19:14:56 +0100

    Setup: TP2

---
+++ core/TP2/src/router/firewall/configure.bash
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+## ==== [ System Packages Installation ] ==== ##
+
+## ==== [ System IP Forwarding ] ==== ##
+
+## ==== [ System Interfaces Configuration ] ==== ##
+
+# IPTables Configuration 
+
+## ==== [ Table filter ] ==== ##
+
+# ---- FORWARD ---- #
+
+# ---- INPUT ---- #
+
+# ---- OUTPUT ---- #
+
+
+
+## ==== [ Table nat ] ==== ##
+
+# ---- PREROUTING ---- #
+
+# ---- OUTPUT ---- #
+
+# ---- POSTROUTING ---- #
+
+
+
+## ==== [ Table mangle ] ==== ##
+
+# ---- PREROUTING ---- #
+
+# ---- FORWARD ---- #
+
+# ---- INPUT ---- #
+
+# ---- OUTPUT ---- #
+
+# ---- POSTROUTING ---- #
+
